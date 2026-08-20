dcd133b738e8394b44fa10b3e17aa113ad1995bd
Amir Nesru <amirnesru054@gmail.com>
2026-08-20 21:09:08 +0300

    fix : fix the path of middleware

---
+++ src/routes/batch.route.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const batchController = require("../controllers/batch.controller");
+const { protect, authorize } = require("../middlewares/auth.middleware");
+
+router.use(protect);
+router.use(authorize("admin"));
+
+router.get("/stats", batchController.getBatchDashboardStats);
+router.get("/", batchController.getAllBatches);
+router.post("/", batchController.createBatch);
+router.get("/:batchId", batchController.getBatchById);
+router.patch("/:batchId", batchController.updateBatch);
+router.delete("/:batchId", batchController.deleteBatch);
+
+module.exports = router;
