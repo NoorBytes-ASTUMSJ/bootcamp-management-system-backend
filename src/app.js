8c4f077d29f2925920b2934deffd220331259adb
Amir Nesru <amirnesru054@gmail.com>
2026-08-20 21:08:34 +0300

    feat: add member,user and bactch route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/auth.routes");
+const batchRoutes = require("./routes/batch.route");
+const userRoutes = require("./routes/user.routes");
+const memberRoutes = require("./routes/member.routes");
 const errorHandler = require("./middlewares/errorHandler.middleware");
 
 const app = express();
@@ -10,6 +13,9 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/batches", batchRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/members", memberRoutes);
 
 // Must be last — catches errors passed via next(error) from every controller
 app.use(errorHandler);
